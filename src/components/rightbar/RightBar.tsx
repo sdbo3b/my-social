48be985a916239bfd4d8a0933bb395565e97a5c4
sdbo <49082008+sdbo3b@users.noreply.github.com>
2021-09-05 20:59:28 -0400

    Add right bar component

--- src/components/rightbar/RightBar.tsx
+++ src/components/rightbar/RightBar.tsx
@@ -1,13 +1,54 @@
-import { createStyles, makeStyles } from "@material-ui/core";
+import {
+  Badge,
+  createStyles,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import React from "react";
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flex: 3.5,
       display: "flex",
     },
+    body: {
+      padding: theme.spacing(2),
+    },
+    birthdayContainer: {
+      display: "flex",
+      alignItems: "center",
+    },
+    birthdayImg: {
+      width: "40px",
+      height: "40px",
+      marginRight: "10px",
+    },
+    birthdayText: {
+      fontWeight: theme.typography.fontWeightLight,
+      fontSize: theme.typography.fontSize,
+    },
+    ad: {
+      width: "100%",
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      borderRadius: "10px",
+    },
+    friendImg: {
+      width: "40px",
+      height: "40px",
+      borderRadius: "50%",
+      objectFit: "cover",
+    },
+    dot: {
+      backgroundColor: "#33eb13",
+    },
+    title: {},
   })
 );
 
@@ -16,7 +57,38 @@ const RightBar: React.FC = () => {
 
   return (
     <Box className={classes.root}>
-      <div>RightBar</div>
+      <Box className={classes.body}>
+        <Box className={classes.birthdayContainer}>
+          <img
+            className={classes.birthdayImg}
+            src="/assets/gift.png"
+            alt="gift"
+          />
+          <span className={classes.birthdayText}>
+            <b>Tom Doe</b> and <b>4 other friends</b> have a birthday today.
+          </span>
+        </Box>
+        <img className={classes.ad} src="/assets/ad.png" alt="" />
+        <h4 className={classes.title}>Online Friends</h4>
+        <List component="div">
+          <ListItem button>
+            <ListItemIcon>
+              <Badge
+                classes={{ dot: classes.dot }}
+                variant="dot"
+                overlap="circular"
+              >
+                <img
+                  className={classes.friendImg}
+                  src="/assets/person/3.jpeg"
+                  alt="friend"
+                />
+              </Badge>
+            </ListItemIcon>
+            <ListItemText primary="David Doe" />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
